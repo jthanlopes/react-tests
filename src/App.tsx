549bed550902a8d4e948263f94ce2d23c2002487
Jonathan dos Santos <jthan.lopes@gmail.com>
2023-03-23 10:53:36 -0300

    feat: sistema de rotas da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { About } from './pages/About'
+import { Home } from './pages/Home'
+import { Layout } from './pages/Layout'
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
